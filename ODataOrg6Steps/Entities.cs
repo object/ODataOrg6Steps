c75b99bb5734f4c8e7366feb72349ad5c8d3d78d
Vagif Abilov <vagif.abilov@gmail.com>
2014-12-14 21:54:27 +0100

    Initial revision.

---
+++ ODataOrg6Steps/Entities.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Spatial;
+
+namespace ODataOrg6Steps
+{
+    enum PersonGender
+    {
+        Male,
+        Female,
+        Unknown,
+    }
+
+    class Person
+    {
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string[] Emails { get; set; }
+        public Location[] AddressInfo { get; set; }
+        public PersonGender Gender { get; set; }
+        public long Concurrency { get; set; }
+
+        public IEnumerable<Person> Friends { get; set; }
+        public IEnumerable<Trip> Trips { get; set; }
+        public IEnumerable<Photo> Photos { get; set; }
+    }
+
+    class People : Person
+    {
+    }
+
+    class Trip
+    {
+        public int TripId { get; set; }
+        public Guid ShareId { get; set; }
+        public string Description { get; set; }
+        public string Name { get; set; }
+        public float Budget { get; set; }
+        public DateTimeOffset StartsAt { get; set; }
+        public DateTimeOffset EndsAt { get; set; }
+        public IList<string> Tags { get; set; }
+
+        public IEnumerable<Photo> Photos { get; set; }
+        public IEnumerable<PlanItem> PlanItems { get; set; }
+    }
+
+    class Photo
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    class PlanItem
+    {
+        public int PlanItemId { get; set; }
+        public string ConfirmationCode { get; set; }
+        public DateTimeOffset StartsAt { get; set; }
+        public DateTimeOffset EndsAt { get; set; }
+        public TimeSpan Duration { get; set; }
+    }
+
+    class PublicTransportation : PlanItem
+    {
+        public string SeatNumber { get; set; }
+    }
+
+    class Flight : PublicTransportation
+    {
+        public string FlightNumber { get; set; }
+        public Airport From { get; set; }
+        public Airport To { get; set; }
+        public Airline Airline { get; set; }
+    }
+
+    class Event : PlanItem
+    {
+        public string Description { get; set; }
+        public EventLocation OccursAt { get; set; }
+    }
+
+    class Airline
+    {
+        public string AirlineCode { get; set; }
+        public string Name { get; set; }
+    }
+
+    class Airport
+    {
+        public string IcaoCode { get; set; }
+        public string IataCode { get; set; }
+        public string Name { get; set; }
+        public AirportLocation Location { get; set; }
+    }
+
+    public class City
+    {
+        public string CountryRegion { get; set; }
+        public string Name { get; set; }
+        public string Region { get; set; }
+    }
+
+    class Location
+    {
+
+        public string Address { get; set; }
+        public City City { get; set; }
+    }
+
+    class AirportLocation : Location
+    {
+        public GeographyPoint Loc { get; set; }
+    }
+
+    class EventLocation : Location
+    {
+        public string BuildingInfo { get; set; }
+    }
+}
---
+++ ODataOrg6Steps/TypedSamples.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Simple.OData.Client;
+using Xunit;
+
+namespace ODataOrg6Steps
+{
+    public class TypedSamples
+    {
+        public TypedSamples()
+        {
+        }
+
+        [Fact]
+        public async Task Step1()
+        {
+            var client = new ODataClient("http://services.odata.org/v4/TripPinServiceRW/");
+            var people = await client
+                .For<People>()
+                .FindEntriesAsync();
+            Assert.Equal(8, people.Count());
+        }
+
+        [Fact]
+        public async Task Step1_WithAnnotations()
+        {
+            var client = new ODataClient("http://services.odata.org/v4/TripPinServiceRW/");
+            var annotations = new ODataFeedAnnotations();
+            var count = 0;
+            var people = await client
+                .For<People>()
+                .FindEntriesAsync(annotations);
+            count += people.Count();
+            while (annotations.NextPageLink != null)
+            {
+                people = await client
+                    .For<People>()
+                    .FindEntriesAsync(annotations.NextPageLink, annotations);
+                count += people.Count();
+            }
+            Assert.Equal(20, count);
+        }
+
+        [Fact]
+        public async Task Step2()
+        {
+            var client = new ODataClient("http://services.odata.org/v4/TripPinServiceRW/");
+            var person = await client
+                .For<People>()
+                .Key("russellwhyte")
+                .FindEntryAsync();
+            Assert.Equal("russellwhyte", person.UserName);
+        }
+
+        [Fact]
+        public async Task Step3()
+        {
+            var client = new ODataClient("http://services.odata.org/v4/TripPinServiceRW/");
+            var people = await client
+                .For<People>()
+                .Filter(x => x.Trips.Any(y=> y.Budget > 3000))
+                .Top(2)
+                .Select(x => new {x.FirstName, x.LastName})
+                .FindEntriesAsync();
+            Assert.Equal(2, people.Count());
+        }
+
+        [Fact]
+        public async Task Step4()
+        {
+            var client = new ODataClient("http://services.odata.org/v4/TripPinServiceRW/");
+            var person = await client
+                .For<People>()
+                .Set(new People()
+                {
+                    UserName = "lewisblack",
+                    FirstName = "Lewis",
+                    LastName = "Black",
+                    Emails = new [] { "lewisblack@example.com" },
+                    AddressInfo = new []
+                    {
+                        new Location()
+                        {
+                            Address = "187 Suffolk Ln.",
+                            City = new City
+                            {
+                                CountryRegion = "United States", 
+                                Name = "Boise", 
+                                Region = "ID"
+                            }
+                        }
+                    },
+                    Gender = PersonGender.Male,
+                    Concurrency = 635519729375200400
+                })
+                .InsertEntryAsync();
+            Assert.NotNull(person);
+        }
+
+        [Fact]
+        public async Task Step5()
+        {
+            var client = new ODataClient("http://services.odata.org/v4/TripPinServiceRW/");
+            var trip = await client
+                .For<People>()
+                .Key("russellwhyte")
+                .NavigateTo<Trip>()
+                .Key(0)
+                .FindEntryAsync();
+            await client
+                .For<People>()
+                .Key("scottketchum")
+                .LinkEntryAsync(trip);
+            var person = await client
+                .For<People>()
+                .Key("scottketchum")
+                .Expand(x => x.Trips)
+                .FindEntryAsync();
+            Assert.True(person.Trips.Any(x => x.Name == trip.Name));
+        }
+
+        [Fact]
+        public async Task Step6()
+        {
+            var client = new ODataClient("http://services.odata.org/v4/TripPinServiceRW/");
+            var people = await client
+                .For<People>()
+                .Key("scottketchum")
+                .NavigateTo<Trip>()
+                .Key(0)
+                .Function("GetInvolvedPeople")
+                .ExecuteAsEnumerableAsync();
+            Assert.Equal(2, people.Count());
+        }
+    }
+}
